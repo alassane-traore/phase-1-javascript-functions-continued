b36a9e902c4b569cc40f98af076522d243c1a504
alassane-traore <alassanet076@gmail.com>
2022-08-22 07:36:48 +0300

    fist commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function saturdayFun(empty= "roller-skate"){
+    console.log(`This Saturday, I want to ${empty}!`);
+}
+
+const mondayWork= function (mondayWork= "go to the office"){
+    console.log (`This Monday, I will ${mondayWork}`);
+}
+function wrapAdjective {
+
+}
